bf3062ef31bb146ddfc3ddf905c0bc074eddbccc
Monjurul Alam <monjurulalam190629@gmail.com>
2024-04-28 16:29:43 +0600

    another delete operation is now Ok

--- index.js
+++ index.js
@@ -59,29 +59,37 @@ async function run() {
             const result = await SpotCollection.findOne(query)
             res.send(result)
         })
-       
-        app.put('/TouristSpots/:id', async(req, res) => {
+
+        app.put('/TouristSpots/:id', async (req, res) => {
             const id = req.params.id;
-            const filter = { _id : new ObjectId(id) };
+            const filter = { _id: new ObjectId(id) };
             const options = { upsert: true };
             const spot1 = req.body;
+            console.log(spot1)
             const updateDoc = {
-              $set: {
-                image : spot1.image,
-                tourist_spot_name : spot1.tourist_spot_name,
-                country_name : spot1.country_name,
-                average_cost : spot1.average_cost,
-                travel_time : spot1.travel_time,
-                location: spot1.location,
-                short_description : spot1.short_description,
-                seasonality : spot1.seasonality,
-                total_visitors_per_year : spot1.total_visitors_per_year
-              },
+                $set: {
+                    image: spot1.image,
+                    tourists_spot_name: spot1.tourist_spot_name,
+                    country_name: spot1.country_name,
+                    average_cost: spot1.average_cost,
+                    travel_time: spot1.travel_time,
+                    location: spot1.location,
+                    short_description: spot1.short_description,
+                    seasonality: spot1.seasonality,
+                    total_visitors_per_year: spot1.total_visitors_per_year
+                },
             };
             const result = await SpotCollection.updateOne(filter, updateDoc, options);
             res.send(result)
-           })
-
+        })
+        ////delete
+        /// delete a id
+        app.delete('/TouristSpots/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await SpotCollection.deleteOne(query)
+            res.send(result)
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
